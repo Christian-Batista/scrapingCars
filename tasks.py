f8581d06a145d66621ee38ada1cc3cc1e2a9c891
ChristianBatista <elcatchon@gmail.com>
2025-03-09 03:21:22 -0400

    Working With the tasks

---
+++ tasks.py
@@ -0,0 +1,11 @@
+from celery import Celery
+from scraper_service import scrape_all_sites
+
+
+app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+
+@app.task
+def test_task():
+    # return "Hello from Celery!"
+    all_cars_data = scrape_all_sites()
+    print(all_cars_data)
